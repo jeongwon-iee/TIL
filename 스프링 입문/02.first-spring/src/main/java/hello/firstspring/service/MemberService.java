7bce97ba4f1a1e56601af73e8919baf2ca80d9fe
jeongwon-iee <garden1117244@gmail.com>
2021-01-21 19:45:39 +0900

    [Update] Add Dipendency Injection

--- 스프링 입문/02.first-spring/src/main/java/hello/firstspring/service/MemberService.java
+++ 스프링 입문/02.first-spring/src/main/java/hello/firstspring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.firstspring.service;
 import hello.firstspring.domain.Member;
 import hello.firstspring.repository.MemberRepository;
 import hello.firstspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
