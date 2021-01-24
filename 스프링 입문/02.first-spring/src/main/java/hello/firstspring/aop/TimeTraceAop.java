3e05016819116327524fd7e64c00ffe18c8ac9ac
jeongwon-iee <garden1117244@gmail.com>
2021-01-24 20:03:03 +0900

    [Add] Add AOP

---
+++ 스프링 입문/02.first-spring/src/main/java/hello/firstspring/aop/TimeTraceAop.java
@@ -0,0 +1,26 @@
+package hello.firstspring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.firstspring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START :" + joinPoint.toString());
+        try {
+            // 다음 메소드로 진행
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END :" + joinPoint.toString() + " " + timeMs + "ms");
+
+        }
+    }
+}
