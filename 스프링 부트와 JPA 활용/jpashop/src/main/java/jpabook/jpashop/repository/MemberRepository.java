c17d9c4aab8cc6028f1d3b3e56007dfbf6f2a0ad
jeongwon-iee <garden1117244@gmail.com>
2021-01-29 14:52:16 +0900

    [Add] Member Service

--- 스프링 부트와 JPA 활용/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ 스프링 부트와 JPA 활용/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -12,12 +13,22 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public Long save(Member member) {
+    public void save(Member member) {
         entityManager.persist(member);
-        return member.getId();
     }
 
-    public Member findById(Long id) {
+    public Member findOne(Long id) {
         return entityManager.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return entityManager.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
