e6885003006e97e788cf82493c47e2f41ce1474c
jeongwon-iee <garden1117244@gmail.com>
2021-03-03 18:14:33 +0900

    test(singleton): add bean singleton test

---
+++ 스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,26 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        // 1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
