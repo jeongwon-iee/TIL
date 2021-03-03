27bec6ffc4f4920395f557e67acdd09c315460cd
jeongwon-iee <garden1117244@gmail.com>
2021-03-03 18:53:54 +0900

    test(singleton): add singleton container test

--- 스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ 스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,5 +33,20 @@ public class SingletonTest {
         SingletonService singletonService2 = SingletonService.getInstance();
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = applicationContext.getBean("memberService", MemberService.class);
+
+        // 2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = applicationContext.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
