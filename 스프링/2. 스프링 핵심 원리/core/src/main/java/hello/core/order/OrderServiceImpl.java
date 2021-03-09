0a1409a00d7e3aa8fb6b3b9779fad61805dd36a9
jeongwon-iee <garden1117244@gmail.com>
2021-03-09 15:45:43 +0900

    fix(OrderServiceImpl): remove getter

--- 스프링/2. 스프링 핵심 원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링/2. 스프링 핵심 원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -25,4 +25,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    // 테스트용
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
