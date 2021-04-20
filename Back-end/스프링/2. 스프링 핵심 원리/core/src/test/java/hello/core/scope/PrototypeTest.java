3ef940d858bf778eabda11580a9a1babf8f01dbe
jeongwon-iee <garden1117244@gmail.com>
2021-04-20 15:29:15 +0900

    feat(scope): add prototype bean scope test

---
+++ Back-end/스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = applicationContext.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        applicationContext.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
