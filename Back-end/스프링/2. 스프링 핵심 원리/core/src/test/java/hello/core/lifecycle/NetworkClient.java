f202c5516fe53d73662346d5a6340786282b6feb
jeongwon-iee <garden1117244@gmail.com>
2021-04-20 12:01:48 +0900

    fix(lifecycle): add bean init & close method

--- Back-end/스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ Back-end/스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +29,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
         disconnect();
     }
 }
