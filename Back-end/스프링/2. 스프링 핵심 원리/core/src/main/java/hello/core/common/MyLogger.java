99dbd922ebcf874a85a525eefbcd81db734ddee7
jeongwon-iee <garden1117244@gmail.com>
2021-04-24 19:16:50 +0900

    feat(web): add web MVC

---
+++ Back-end/스프링/2. 스프링 핵심 원리/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,34 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+    private String uuid;
+    private String requestUrl;
+
+    public void setRequestUrl(String requestUrl) {
+        this.requestUrl = requestUrl;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestUrl + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "]" + "[" + requestUrl + "] request scope bean create : " + this);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("[" + uuid + "]" + "[" + requestUrl + "] request scope bean close : " + this);
+    }
+}
