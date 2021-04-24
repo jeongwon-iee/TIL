33ce80b7a0ba0ec2562fc21091075057cfdb9d80
jeongwon-iee <garden1117244@gmail.com>
2021-04-24 18:14:05 +0900

    feat(scope): add prototype bean with singleton bean using provider

--- Back-end/스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest3.java
+++ Back-end/스프링/2. 스프링 핵심 원리/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest3.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,10 +42,10 @@ public class SingletonWithPrototypeTest3 {
     @Scope("singleton")
     static class ClientBean {
         @Autowired
-        ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
